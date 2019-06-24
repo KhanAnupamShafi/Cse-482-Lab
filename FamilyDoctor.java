19d2d6d9b50e8ef0f6b891da61e6b3997203ffd5
Khan Anupam Shafi <41741660+KhanAnupamShafi@users.noreply.github.com>
2019-06-24 08:46:00 +0600

    Add files via upload

---
+++ FamilyDoctor.java
@@ -0,0 +1,9 @@
+package cse482.diptu.lab1;
+
+public class FamilyDoctor extends Doctor{
+	boolean makesHouseCalls;
+	
+	void giveAdvice() {
+	System.out.println("Family doctor : give homespun advice"); 
+	}
+}
